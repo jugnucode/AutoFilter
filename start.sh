dc56eaedb9484065b800d3134148cfcaae83bad3
jugnucode <101200766+jugnucode@users.noreply.github.com>
2022-12-03 06:52:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CyberStainBotbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/jugnucode/AutoFilter/tree/koyeb /AutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /AutoFilter
 fi
-cd /Eva
+cd /AutoFilter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
